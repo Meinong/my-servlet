2ebff295c228fd074934eceb5d0bde01ca75873e
zhu <710738830@qq.com>
2023-06-06 11:51:41 +0800

    servlet 获取请求参数值

--- src/main/java/com/zhz/servlet/HelloServlet.java
+++ src/main/java/com/zhz/servlet/HelloServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/")
 public class HelloServlet extends HttpServlet {
@@ -16,9 +17,11 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
+        String name = req.getParameter("name");
+
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
-        writer.write("<h1> Hello , world <h1>");
+        writer.write("<h1> name:"+ name +" <h1>");
         writer.flush();
     }
 }
